ba79e0e11f032f11c4e4a46b3e3f46bd98eab6bc
Chewah <chewah@users.noreply.github.com>
2020-02-15 13:28:19 -0500

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,7 +1,5 @@
 // This is the "Offline page" service worker
 
-var self = this;
-
 const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
