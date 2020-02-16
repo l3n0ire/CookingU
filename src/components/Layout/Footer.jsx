24cbfbf23ed573a59805483878db6ac222b0cf44
Chewah <chewah@users.noreply.github.com>
2020-02-15 23:49:50 -0500

    Footer Changes

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -8,27 +8,9 @@ function Footer() {
         <Fragment>
             <footer>
                 <div class="footer-container">
-                <div className="box-left">
-                    <h1>CookingU</h1>
-                    <p>
-                    1265 Military Trail <br/>
-                    Toronto, ON <br/>
-                    M1C 1A4
-                    </p>
-                    <p><br/>[insert social media] </p>
-                    <br/>
+                
                     <p>&copy; CookingU {(new Date().getFullYear())}</p>
-                </div>
-                <div className="box-right">
-                    <h1>Links</h1>
-                    <ul>
-                        <li><Link className="link-style" to='/'>Home</Link></li>
-                        <li><Link className="link-style" to='/recipes'>Recipes</Link></li>
-                        <li><Link className="link-style" to='/about'>About</Link></li>
-
-                    </ul>
-                </div>
-                </div>
+                </div>            
             </footer>
         </Fragment>
     )
