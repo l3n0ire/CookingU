ea6760c750622c74828be3fd8373d77168d4ee8f
Chidubem Ogbudibe <chidubem@live.ca>
2020-02-16 07:53:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,9 @@
-/*'use strict';
 require('dotenv').config({ path: 'variables.env' });
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const serverless = require('serverless-http');
-const cors = require('cors');
-const processMessage = require('../process-message');
-
-const app = express();
-const router = express.Router();
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/chat', (req, res) => {
-  const { message } = req.body;
-  processMessage(message);
-});
-
-app.set('port', process.env.PORT || 5000);
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-
-module.exports = app;
-module.exports.handler = serverless(app);
-*/
-require('dotenv').config({ path: 'variables.env' });
-
-const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const serverless = require('serverless-http');
 const processMessage = require('./process-message');
 
 const app = express();
